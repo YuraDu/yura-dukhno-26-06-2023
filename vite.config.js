e63f07314248cf9e34266ed5f662538c6a99f1f7
yuradukhno <yura.dukhno@gmail.com>
2023-07-03 10:43:50 +0300

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://github.com/YuraDu/yura-dukhno-26-06-2023",
+  base: "/yura-dukhno-26-06-2023/",
   plugins: [react()],
 });
