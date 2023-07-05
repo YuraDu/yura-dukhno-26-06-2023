056d6a628f1e0fab5d0706250dbae680f5f9fdfa
yuradukhno <yura.dukhno@gmail.com>
2023-07-05 10:23:15 +0300

    erro animation fixed for descktop full viewport

--- src/Components/ErrorsPopUp/ErrorsPopUp.jsx
+++ src/Components/ErrorsPopUp/ErrorsPopUp.jsx
@@ -2,22 +2,58 @@ import { useDispatch, useSelector } from "react-redux";
 import "./ErrorsPopUp.css";
 import { useEffect } from "react";
 import { addError } from "../../Redux/reducers/generalSlice";
+import { useSpring, animated } from "@react-spring/web";
 
 const ErrorsPopUp = () => {
+  const [springs, api] = useSpring(() => ({
+    from: {
+      x: 0,
+      y: 0,
+    },
+  }));
   const dispatch = useDispatch();
   const error = useSelector(state => state.general.error);
 
   useEffect(() => {
+    if (error) {
+      api.start({
+        from: {
+          x: -160,
+          y: 0,
+        },
+        to: {
+          x: -160,
+          y: -70,
+        },
+      });
+    }
+    const timeOut = setTimeout(() => {
+      if (error) {
+        api.start({
+          from: {
+            x: -160,
+            y: -70,
+          },
+          to: {
+            x: -160,
+            y: 0,
+          },
+        });
+      }
+    }, 2000);
     const errorTimeout = setTimeout(() => {
       dispatch(addError(""));
     }, 3000);
-    return () => clearTimeout(errorTimeout);
+    return () => {
+      clearTimeout(timeOut);
+      clearTimeout(errorTimeout);
+    };
   }, [error]);
 
   return (
-    <div className="errors__container">
-      {error && <div className="error">{error}</div>}
-    </div>
+    <animated.div style={{ ...springs }} className="errors__container">
+      <div className="error">{error}</div>
+    </animated.div>
   );
 };
 export default ErrorsPopUp;
